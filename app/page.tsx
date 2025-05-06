406b188b1cc9dd98e0e72160ccdb3a6d958481fc
ntphuc0987 <truelove309@gmail.com>
2025-05-06 08:50:28 +0700

    fixed app->page

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ const BlogPosts = async () => {
   const data = await getData();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {data.map((item: any) => (
+      {data.map((item) => (
         <BlogPostCard data={item} key={item.id} />
       ))}
     </div>
